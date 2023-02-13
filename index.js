06e30772b7ffbd14258410e1170723a47f3b4f9a
HuwanJ <97149792+HuwanJ@users.noreply.github.com>
2023-02-13 18:59:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(".card-front").click(function() {
+    $(".text-one").delay(5500).animate({
+        opacity:'1'
+      }, 1000);
+       $(".text-one").delay(6500).animate({
+        opacity:'1'
+      }, 1000);
+        
+    });
